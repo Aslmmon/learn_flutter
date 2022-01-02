5aad38f49a826ad074aefbe98608344c94d47f6f
code95 <aslm.monir@code95>
2022-01-02 10:57:06 +0200

    initialize learn_flutter

---
+++ lib/networking/NetworkingApp.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class NetworkingApp extends StatelessWidget {
+  const NetworkingApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Container(
+      child: Text("Networking is here"),
+    ));
+  }
+}
